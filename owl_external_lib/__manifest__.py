1afa03c00621f3a55af6ad73e561e5244f874426
Han Zaw Nyein <hanzaw@Hans-MacBook-Air.local>
2024-03-06 00:52:52 +0630

    [add] assets

--- owl_external_lib/__manifest__.py
+++ owl_external_lib/__manifest__.py
@@ -5,7 +5,10 @@
     ],
     "assets": {
         "web.assets_backend": [
-            "owl_external_lib/static/src/**/*",
+            "owl_external_lib/static/src/owl_external_lib/*",
+        ],
+        "owl_external_lib.assets_owl_external_lib": [
+            "owl_external_lib/static/src/assets/*.scss"
         ],
     },
     "license": "LGPL-3"
--- owl_external_lib/static/src/owl_external_lib/owl_external_lib.js
+++ owl_external_lib/static/src/owl_external_lib/owl_external_lib.js
@@ -2,11 +2,18 @@
 
 import {registry} from "@web/core/registry";
 import {Component, onWillStart} from "@odoo/owl";
+import {loadCSS, loadJS,loadBundle} from "@web/core/assets";
 
 
 class OwlExternalLib extends Component {
     setup() {
-        onWillStart(async () => {});
+        onWillStart(async () => {
+            await loadCSS('https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css');
+            await loadCSS('https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap');
+            await loadCSS('https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/7.1.0/mdb.min.css');
+            await loadJS('https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/7.1.0/mdb.umd.min.js');
+            await loadBundle('owl_external_lib.assets_owl_external_lib');
+        });
     }
 }
 
